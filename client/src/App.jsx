e3c6f188a80ea38907ae4d0a482c25a9a442cf71
[Khushboo Chaudhary] <khushboochaudhary0511@gmail.com>
2024-04-27 13:08:44 +0530

    created header section

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <h3 className="text-xl font-semibold text-red-300 ">My blog app</h3>
+    <span className="text-3xl font-extrabold items-center text-pink-700">Hiii there!</span>
+    </>
+  )
+}
