eb92e513a5a90302cdff5a3951136f1f8d532440
[Khushboo Chaudhary] <khushboochaudhary0511@gmail.com>
2024-04-27 17:34:53 +0530

    connect to cloud database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDB is connected')
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express()
 const PORT = 3000
